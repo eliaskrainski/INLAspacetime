9ca090d4dcc93e5fdd6c13c0527ece4255c2edd8
Elias T Krainski <eliaskrainski@gmail.com>
2022-02-20 11:21:09 +0300

    add initial structure

---
+++ R/INLAspacetime.R
@@ -0,0 +1,13 @@
+#' Functions for easier code to fit of some models with INLA.
+#'
+#' This package main pourpose is to provide user friendly functions
+#' to fit temporal, spatial and spatio-temporal models using the
+#' INLA software available at www.r-inla.org
+#'
+#' @return opens the Vignettes directory on a browser
+#' @export
+
+INLAspacetime <- function() {
+  print("Welcome to the INLAspacetime package!")
+  browseVignettes("INLAspacetime")
+}
