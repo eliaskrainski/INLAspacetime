5248b7383438a14e7974aee373eb566204518462
eliaskrainski <eliaskrainski@gmail.com>
2022-12-07 08:34:08 +0300

    inclue 'n' (as negative) when returning negated scores

--- R/stats.inla.R
+++ R/stats.inla.R
@@ -1,9 +1,9 @@
 #' To retrieve goodness of fit statistics from an inla output object.
 #'
-#' Exctracts dic, waic and cpo from an inla output and
-#' computes po, mse, mae, crps and scrps for a given input.
+#' Exctracts DIC, WAIC and CPO from an inla output and
+#' computes PO, MSE, MAE, CRPS and SCRPS for a given input.
 #' A summary is applied considering the user imputed function,
-#' which by default is the mean.
+#' which by default is the mean. See Details and Warning!
 #'
 #' @param m an inla output object.
 #' @param i an index to subset the estimated values.
@@ -11,13 +11,13 @@
 #' @param fsummarize the summary function,
 #' the default is [base::mean()].
 #' @section Details:
-#'  When computing the po, crps, and scrps scores it assumes a
+#'  When computing the PO, CRPS, and SCRPS scores it assumes a
 #'  Gaussian predictive distribution with mean equal to the
 #'  posterior mean and variance equal to the sum of the
 #'  posterior fitted value variance and the inverse of the
 #'  observation precision posterior mean.
 #' @section Warning:
-#'  For cpo, po, crps and scrps what is returned is the
+#'  For CPO, PO, CRPS and SCRPS what is returned is the
 #'  negative of the applied summary.
 #' @return a vector with the extracted statistics
 #' @export
@@ -36,20 +36,20 @@ stats.inla <- function(m, i=NULL, y, fsummarize=mean) {
     if(is.null(i))
       i <- 1:length(m$dic$local.dic)
     r <- c(
-      dic=fsummarize(m$dic$local.dic[i]),
-      waic=fsummarize(m$waic$local.waic[i]),
-      po=-fsummarize(dnorm(
+      DIC=fsummarize(m$dic$local.dic[i]),
+      WAIC=fsummarize(m$waic$local.waic[i]),
+      nPO=-fsummarize(dnorm(
         y[i], m$summary.fitted.value$mean[i],
         sqrt(m$summary.fitted.value$sd[i]^2 +
               1/m$summary.hyperpar$mean[1]), log=TRUE)),
-      cpo=-fsummarize(log(m$cpo$cpo[i])),
-      mse=fsummarize((m$summary.fitted.value$mean[i]-y[i])^2),
-      mae=fsummarize(abs(m$summary.fitted.value$mean[i]-y[i])),
-      crps=-fsummarize(crps.g(
+      nCPO=-fsummarize(log(m$cpo$cpo[i])),
+      MSE=fsummarize((m$summary.fitted.value$mean[i]-y[i])^2),
+      MAE=fsummarize(abs(m$summary.fitted.value$mean[i]-y[i])),
+      nCRPS=-fsummarize(crps.g(
                  y[i], m$summary.fitted.value$mean[i],
                  sqrt(m$summary.fitted.value$sd[i]^2 +
                  1/m$summary.hyperpar$mean[1]))),
-      scrps=-fsummarize(scrps.g(
+      nSCRPS=-fsummarize(scrps.g(
         y[i],
         m$summary.fitted.val$mean[i],
         sqrt(m$summary.fitted.val$sd[i]^2 +
