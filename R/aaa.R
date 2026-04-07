df81f9a088c02f34a31ab1744476f93aeb86eadc
Elias T Krainski <eliaskrainski@gmail.com>
2026-04-07 12:29:42 +0300

    aaa.R to work

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,5 @@
-
+#' Internal to make NAMESPACE
+#' @noRd
 #' @importFrom methods as
 #' @importFrom stats dnorm pnorm complete.cases
 #' @importFrom fmesher fm_manifold fm_manifold_dim fm_fem
@@ -6,3 +7,4 @@
 #' @importFrom INLAtools is.zero packageCheck cgeneric_shlib
 #' @importFrom INLAtools Sparse upperPadding cgenericBuilder
 #' @importFrom INLAtools cgeneric prec graph mu initial prior
+NULL
