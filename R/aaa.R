6e7d838ba08a1039dc11373230e47c1d3793804e
Elias T Krainski <eliaskrainski@gmail.com>
2026-04-07 12:18:25 +0300

    add R/aaa.R to importFrom

---
+++ R/aaa.R
@@ -0,0 +1,8 @@
+
+#' @importFrom methods as
+#' @importFrom stats dnorm pnorm complete.cases
+#' @importFrom fmesher fm_manifold fm_manifold_dim fm_fem
+#' @importFrom Matrix Diagonal sparseMatrix
+#' @importFrom INLAtools is.zero packageCheck cgeneric_shlib
+#' @importFrom INLAtools Sparse upperPadding cgenericBuilder
+#' @importFrom INLAtools cgeneric prec graph mu initial prior
